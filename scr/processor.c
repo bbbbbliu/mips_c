c5c0cb84ac8f96f39572ce3a9ed2c774598151f1
Byron Liu <u1175421@utah.edu>
2026-06-17 18:29:13 -0600

    made src and test folders, need to add files

---
+++ scr/processor.c
@@ -0,0 +1,21 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+struct mipsProcessor {
+  /* General Purpose Registers
+   * Не забудь:
+   * $0 всегда возвращает 0!
+   * $31 (називается $ra, return address) есть обратный адрес
+   * */
+  uint32_t gpr[32];
+
+  /* Multiply/Divide */
+  uint32_t hi;
+  uint32_t lo;
+
+  /* Program Counters*/
+  uint32_t pc;
+  uint32_t pc_next; /* необязательный для branch-delay */
+};
